e7cd5df009e22e3da9496f97cb93760452706e8e
SNishanth2202 <s.nishanth2202@gmail.com>
2026-04-23 20:33:39 +0530

    push

--- 24U020020_Assignment 12.php
+++ 24U020020_Assignment 12.php
@@ -2,12 +2,13 @@
 session_start();
 
 // Database configuration
-$db_host = 'localhost';
-$db_user = 'root';
-$db_pass = '';
-$db_name = 'itc2026_ass10';
+$db_host = getenv("DB_HOST") ?: 'localhost';
+$db_user = getenv("DB_USER") ?: 'root';
+$db_pass = getenv("DB_PASS") ?: '';
+$db_name = getenv("DB_NAME") ?: 'itc2026_ass10';
+$db_port = getenv("DB_PORT") ?: '3306';
 
-$conn = @new mysqli($db_host, $db_user, $db_pass, $db_name);
+$conn = @new mysqli($db_host, $db_user, $db_pass, $db_name, (int)$db_port);
 $db_connected = true;
 
 if ($conn->connect_error) {
